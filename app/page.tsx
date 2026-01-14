2e52c44dd5b424f2711316ce7331102be5f88399
Kangdy <canfrog@naver.com>
2026-01-14 23:45:11 +0900

    feat: 디자인 시스템 적용을 위한 Pretendard 폰트 도입 - Pretendard 폰트 파일 추가 (Thin ~ Black) - globals.css 내 @font-face 및 @theme 변수 설정 - Tailwind v4 폰트 패밀리 구성 완료

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
 export default function Home() {
   return (
-    <div className="flex justify-center">
-      <h1 className="text-2xl font-bold">Hello world!</h1>
+    <div className="flex h-screen flex-1 flex-col items-center justify-center gap-4 border bg-slate-950 text-white">
+      <h1 className="font-pretendard-thin text-2xl">안녕, 세계!!</h1>
+      <h1 className="font-pretendard-extralight text-2xl">안녕, 세계!!</h1>
+      <h1 className="font-pretendard-light text-2xl">안녕, 세계!!</h1>
+      <h1 className="font-pretendard-regular text-2xl">안녕, 세계!!</h1>
+      <h1 className="font-pretendard-medium text-2xl">안녕, 세계!!</h1>
+      <h1 className="font-pretendard-semibold text-2xl">안녕, 세계!!</h1>
+      <h1 className="font-pretendard-bold text-2xl">안녕, 세계!!</h1>
+      <h1 className="font-pretendard-extrabold text-2xl">안녕, 세계!!</h1>
+      <h1 className="font-pretendard-black text-2xl">안녕, 세계!!</h1>
     </div>
   );
 }
