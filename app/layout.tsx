9ce25289b84494365c3e7a1dae77270af6c74194
Kangdy <canfrog@naver.com>
2026-01-26 00:13:34 +0900

    refactor: 폴더 구조 정리

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import Script from 'next/script';
 import localFont from 'next/font/local';
 import Header from '../components/header';
 import Footer from '../components/footer';
-import GlobalModal from '@/components/globalModal';
+import GlobalModal from '@/components/modal/globalModal';
 
 const pretendard = localFont({
   src: [
