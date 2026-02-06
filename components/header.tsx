1450a80261c15f11ac5cc2bb61d4a9822c8c4438
Shineast <canfrog@naver.com>
2026-02-06 23:25:56 +0900

    feat: 헤더 컴포넌트 모임 만들기 버튼 수정

--- components/header.tsx
+++ components/header.tsx
@@ -2,10 +2,10 @@
 
 import Link from 'next/link';
 import Image from 'next/image';
-// import { useOpenModal } from '@/hooks/useOpenModal';
+import { usePathname } from 'next/navigation';
 
 const Header = () => {
-  // const openModal = useOpenModal();
+  const pathname = usePathname();
 
   return (
     <header className="border-gray-1 top-0 right-0 left-0 flex h-15 items-center justify-center border-b bg-white">
@@ -13,18 +13,17 @@ const Header = () => {
         <Link href="/" className="shrink-0">
           <Image src="/logo.svg" alt="Mingling Logo" width={112} height={40} priority />
         </Link>
-        <nav className="hidden items-center gap-2.5 md:flex">
-          <Link href="/" className="text-gray-5 p-2 text-[16px]">
+
+        <nav className="flex items-center gap-2.5">
+          <Link href="/" className="text-gray-5 hidden p-2 text-[16px] md:block">
             문의하기
           </Link>
-          {/* <button
-            type="button"
-            onClick={(e) => openModal('FEEDBACK', undefined, e)}
-            className="text-gray-5 p-2 text-[16px] transition-colors hover:text-gray-900"
+          <Link
+            href="/create"
+            className="bg-blue-5 hover:bg-blue-8 flex w-25 items-center justify-center rounded-[8px] p-2 text-[14px] font-semibold text-white transition-colors"
           >
-            피드백남기기
-          </button> */}
-          <button type="button" className=" p-2 text-[14px] font-semibold bg-blue-5 text-white rounded-[8px] w-25 cursor-pointer hover:bg-blue-8 transition-colors">모임 만들기</button>
+            모임 만들기
+          </Link>
         </nav>
       </div>
     </header>
